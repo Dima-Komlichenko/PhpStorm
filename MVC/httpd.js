ff313adc5707ba3464cb1545ecfe4077bc7b00e0
35144 <35Ua83Zw>
2021-09-13 17:44:39 +0300

    4

--- MVC/httpd.js
+++ MVC/httpd.js
@@ -3,6 +3,9 @@ const app = express();
 
 const controller = require("./controllers/simpleController");
 
+// Установить каталог для статических файлов
+app.use(express.static("public"));
+
 app.route("/1") // обработка такого-то запроса
     .get(controller.get)
     .post(controller.post);
---
+++ MVC/public/index.js
@@ -0,0 +1,1 @@
+response.write("<p>MVC Public Index</p>");
